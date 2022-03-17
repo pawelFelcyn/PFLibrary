3e190d9caf79fe3ca5dfa6dc9c6bef50df31430f
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-17 13:08:21 +0100

    Add DependencyInjection
    This is a static class for IServiceCollection extensions.
    Implemented method AddDefaultAuthentication.

---
+++ AspNetAuthentication/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using AspNetAuthentication.Settings;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace AspNetAuthentication
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDefaultAuthentication(this IServiceCollection services, IAuthenticationSettings authenticationSettings)
+        {
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.SaveToken = true;
+                    cfg.TokenValidationParameters = new()
+                    {
+                        ValidAudience = authenticationSettings.JwtIssuer,
+                        ValidIssuer = authenticationSettings.JwtIssuer,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey))
+                    };
+                });
+
+            return services;
+        }
+    }
+}
